95ba96e67c78b4ad6cf8b4b0358090ce4c0aae82
Jayden Tactay <jaydent22@gmail.com>
2026-01-08 16:20:11 -0800

    Added loading indicator

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className="flex items-center justify-center h-full">
+            <div className="w-16 h-16 border-4 border-emerald-500 border-t-transparent border-solid rounded-full animate-spin"></div>
+        </div>
+    );
+}
+
+export default Spinner;
