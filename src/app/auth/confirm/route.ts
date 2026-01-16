e0a55ddcf5bcc3698384480486d6dd7ea7b7acb3
Jayden Tactay <jaydent22@gmail.com>
2026-01-16 15:59:27 -0800

    Updated route.ts

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,14 +1,18 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import { createClient } from "../../../lib/supabase/server";
-import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const token_hash = searchParams.get("token_hash");
     const type = searchParams.get("type") as EmailOtpType | null;
-    const next = searchParams.get("next") ?? "/";
+    const next = searchParams.get("next") ?? "/dashboard";
+
+    const redirectTo = request.nextUrl.clone();
+    redirectTo.pathname = next;
+    redirectTo.searchParams.delete("token_hash");
+    redirectTo.searchParams.delete("type");
 
     if (token_hash && type) {
         const supabase = await createClient();
@@ -18,9 +22,11 @@ export async function GET(request: NextRequest) {
             token_hash,
         })
         if (!error) {
-            redirect(next)
+            redirectTo.searchParams.delete("next");
+            return NextResponse.redirect(redirectTo);
         }
     }
 
-    redirect("/login");
+    redirectTo.pathname = "/error";
+    return NextResponse.redirect(redirectTo);
 }
