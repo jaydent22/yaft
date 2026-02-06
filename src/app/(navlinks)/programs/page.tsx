27e2421723973bb69d7e3965cf70e656ee509824
Jayden Tactay <jaydent22@gmail.com>
2026-02-06 15:35:59 -0800

    Empty description UI update

--- src/app/(navlinks)/programs/page.tsx
+++ src/app/(navlinks)/programs/page.tsx
@@ -15,14 +15,14 @@ export default async function Programs() {
   data?.sort((a, b) => (a.created_at! > b.created_at! ? 1 : -1));
   console.log(data);
 
-  function formatDays(day: { name: string; day_number: number }[]) {
-    let days: any = [];
-    const sortedDay = day.sort((a, b) => a.day_number - b.day_number);
-    sortedDay.forEach((d) => {
-      d.name === "Rest Day" ? days.push("R") : days.push("E");
-    })
-    return days.join("-");
-  }
+  // function formatDays(day: { name: string; day_number: number }[]) {
+  //   let days: any = [];
+  //   const sortedDay = day.sort((a, b) => a.day_number - b.day_number);
+  //   sortedDay.forEach((d) => {
+  //     d.name === "Rest Day" ? days.push("R") : days.push("E");
+  //   })
+  //   return days.join("-");
+  // }
 
   return (
     <div className="text-center space-y-2 md:space-y-4">
@@ -49,12 +49,18 @@ export default async function Programs() {
               <h2 className="text-2xl font-bold text-foreground mb-2">
                 {program.name}
               </h2>
-              <p className="text-foreground mb-4">
-                {program.description}
-              </p>
-              <p className="text-foreground font-medium">
+              {program.description.length > 0 ? (
+                <p className="text-foreground mb-4">
+                  {program.description}
+                </p>
+              ) : (
+                <p className="text-foreground mb-4 italic">
+                  No description provided.
+                </p>
+              )}
+              {/* <p className="text-foreground font-medium">
                 {formatDays(program.program_days)}
-              </p>
+              </p> */}
               <a
                 href={`/programs/${program.id}`}
                 className="mt-6 inline-block px-4 py-2 bg-accent text-white rounded hover:bg-accent-dark"
