3d7388dc5df66ac149d1f77cbf5342ffd6de2f08
Jayden Tactay <jaydent22@gmail.com>
2026-02-06 17:14:13 -0800

    Added program sorting

--- src/app/(navlinks)/programs/page.tsx
+++ src/app/(navlinks)/programs/page.tsx
@@ -1,5 +1,6 @@
 "use server";
 import { createClient } from "../../../lib/supabase/server";
+import ProgramList from "../../../components/programs/ProgramList";
 
 export default async function Programs() {
   const supabase = await createClient();
@@ -12,8 +13,7 @@ export default async function Programs() {
     .from("programs")
     .select("*, program_days(name, day_number)")
     .eq("user_id", user?.id);
-  data?.sort((a, b) => (a.created_at! > b.created_at! ? 1 : -1));
-  console.log(data);
+  data?.sort((a, b) => (a.last_modified! > b.last_modified! ? -1 : 1));
 
   // function formatDays(day: { name: string; day_number: number }[]) {
   //   let days: any = [];
@@ -24,52 +24,5 @@ export default async function Programs() {
   //   return days.join("-");
   // }
 
-  return (
-    <div className="text-center space-y-2 md:space-y-4">
-      <h1 className="text-4xl font-bold text-accent mb-4">Programs</h1>
-
-      {data && data.length == 0 && (
-        <p className="text-lg text-foreground">
-          You have no programs yet. Create a new program to get started!
-        </p>
-      )}
-      <a
-        href="/programs/new"
-        className="mt-6 inline-block px-4 py-2 bg-accent text-white rounded hover:bg-accent-dark"
-      >
-        Create New Program
-      </a>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {data &&
-          data.map((program) => (
-            <div
-              key={program.id}
-              className="border border-border rounded-lg p-4 hover:shadow-lg transition-shadow"
-            >
-              <h2 className="text-2xl font-bold text-foreground mb-2">
-                {program.name}
-              </h2>
-              {program.description.length > 0 ? (
-                <p className="text-foreground mb-4">
-                  {program.description}
-                </p>
-              ) : (
-                <p className="text-foreground mb-4 italic">
-                  No description provided.
-                </p>
-              )}
-              {/* <p className="text-foreground font-medium">
-                {formatDays(program.program_days)}
-              </p> */}
-              <a
-                href={`/programs/${program.id}`}
-                className="mt-6 inline-block px-4 py-2 bg-accent text-white rounded hover:bg-accent-dark"
-                >
-                View Program
-              </a>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
+  return <ProgramList initialPrograms={data || []} />;
 }
---
+++ src/components/programs/ProgramList.tsx
@@ -0,0 +1,183 @@
+"use client";
+
+import { useState } from "react";
+import AnchoredMenu from "../AnchoredMenu";
+
+const ProgramList = ({ initialPrograms }: { initialPrograms: any[] }) => {
+  const [programs, setPrograms] = useState(initialPrograms);
+  const [currentSort, setCurrentSort] = useState<
+    "created_at" | "last_modified" | "name"
+  >("last_modified");
+  const [isDescending, setIsDescending] = useState(true);
+
+  function sortPrograms(criteria: "created_at" | "last_modified" | "name") {
+    let sorted: any[] = [];
+    if (criteria === currentSort) {
+      // If already sorted by this criteria, reverse the order
+      sorted = [...programs].reverse();
+      setIsDescending(!isDescending);
+    } else {
+      sorted = [...programs].sort((a, b) => {
+        if (criteria === "name") {
+          return b.name.localeCompare(a.name);
+        } else {
+          return (
+            new Date(b[criteria]).getTime() - new Date(a[criteria]).getTime()
+          );
+        }
+      });
+    }
+    setPrograms(sorted);
+    setCurrentSort(criteria);
+  }
+
+  function handleProgramDelete(programId: string) {
+    setPrograms((prevPrograms) =>
+      prevPrograms.filter((program) => program.id !== programId)
+    );
+  }
+
+  return (
+    <div className="text-center space-y-2 md:space-y-4">
+      {programs && programs.length == 0 ? (
+        <>
+          <h1 className="text-4xl font-bold text-accent mb-4">Programs</h1>
+          <p className="text-lg text-foreground">
+            You have no programs yet. Create a new program to get started!
+          </p>
+          <a
+            href="/programs/new"
+            className="mt-6 inline-block px-4 py-2 bg-accent text-white rounded hover:bg-accent-hover"
+          >
+            Create New Program
+          </a>
+        </>
+      ) : (
+        <>
+          <div className="flex items-center justify-between mb-4">
+            <h1 className="text-4xl font-bold text-accent">Programs</h1>
+            <div className="flex space-x-2">
+              <AnchoredMenu
+                button={
+                  <div className="flex items-center">
+                    <p className="px-2 py-1 text-foreground rounded">Sort By</p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d={
+                          isDescending
+                            ? `M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25`
+                            : `M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12`
+                        }
+                      />
+                    </svg>
+                  </div>
+                }
+              >
+                {({ closeMenu }) => (
+                  <div className="flex flex-col p-4 space-y-2">
+                    <button
+                      type="button"
+                      className={`px-2 py-1 rounded-md text-left ${
+                        currentSort === "last_modified"
+                          ? "bg-surface-hover"
+                          : ""
+                      } hover:bg-surface-hover active:bg-surface-active`}
+                      onClick={() => {
+                        sortPrograms("last_modified");
+                        closeMenu();
+                      }}
+                    >
+                      Last Modified
+                    </button>
+                    <button
+                      type="button"
+                      className={`px-2 py-1 rounded-md text-left ${
+                        currentSort === "created_at" ? "bg-surface-hover" : ""
+                      } hover:bg-surface-hover active:bg-surface-active`}
+                      onClick={() => {
+                        sortPrograms("created_at");
+                        closeMenu();
+                      }}
+                    >
+                      Creation Date
+                    </button>
+                    <button
+                      type="button"
+                      className={`px-2 py-1 rounded-md text-left ${
+                        currentSort === "name" ? "bg-surface-hover" : ""
+                      } hover:bg-surface-hover active:bg-surface-active`}
+                      onClick={() => {
+                        sortPrograms("name");
+                        closeMenu();
+                      }}
+                    >
+                      Name
+                    </button>
+                  </div>
+                )}
+              </AnchoredMenu>
+              <a
+                href="/programs/new"
+                className="px-4 py-2 bg-accent text-white rounded hover:bg-accent-dark"
+              >
+                Create New Program
+              </a>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {programs &&
+              programs.map((program) => (
+                <div
+                  key={program.id}
+                  className="border border-border rounded-lg p-4 hover:shadow-lg transition-shadow"
+                >
+                  <h2 className="text-2xl font-bold text-foreground mb-2">
+                    {program.name}
+                  </h2>
+                  {program.description.length > 0 ? (
+                    <p className="text-foreground mb-4">
+                      {program.description}
+                    </p>
+                  ) : (
+                    <p className="text-foreground mb-4 italic">
+                      No description provided.
+                    </p>
+                  )}
+                  {/* <p className="text-foreground font-medium">
+                {formatDays(program.program_days)}
+              </p> */}
+                  <a
+                    href={`/programs/${program.id}`}
+                    className="mt-6 inline-block px-4 py-2 bg-accent text-white rounded hover:bg-accent-dark"
+                  >
+                    View Program
+                  </a>
+                  {/* <div className="flex items-center justify-between mx-2 mt-2"> */}
+                  <div className="w-full text-left space-y-1 pt-2">
+                    <p className="text-xs italic text-foreground-muted">
+                      Created: {new Date(program.created_at).toLocaleString()}
+                    </p>
+                    <p className="text-xs italic text-foreground-muted">
+                      Last Modified:{" "}
+                      {new Date(program.last_modified).toLocaleString()}
+                    </p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProgramList;
