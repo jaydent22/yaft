075ca3d7842fe72374d69e6d9b126dadd6c2d4d8
Jayden Tactay <jaydent22@gmail.com>
2026-02-11 05:31:32 -0800

    Customizable bottom sheet sizing

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -2,23 +2,30 @@ const BottomSheet = ({
   isOpen,
   onClose,
   children,
+  size = "auto",
+  className,
 }: {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  size: "content" | "auto";
+  className?: string;
 }) => {
   if (!isOpen) return null;
 
   return (
-    <div
-      className="fixed inset-0 z-50 flex items-end justify-center bg-black bg-opacity-50"
-      onClick={onClose}
-    >
-      <div
-        className="w-full rounded-t-lg bg-background p-4"
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
+    <div className="fixed inset-0 z-50 bg-black/75" onClick={onClose}>
+      <div className="absolute inset-0 flex items-end justify-center">
+        <div
+          className={`w-full rounded-t-2xl bg-background p-4 border border-border ${
+            size === "auto"
+              ? "min-h-[25dvh] max-h-[75dvh] flex flex-col"
+              : "h-auto max-h-[75dvh]"
+          } ${className ?? ""}`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/components/exercises/ExerciseSearch/FilterBottomSheet.tsx
+++ src/components/exercises/ExerciseSearch/FilterBottomSheet.tsx
@@ -29,15 +29,33 @@ const FilterBottomSheet = () => {
           </svg>
         </div>
       </button>
-      <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <p>test</p>
-        <div className="mt-4 mb-4 text-center">
-          <div className="flex justify-center items-center space-x-4">
-            <button type="button" className="px-2 py-1 rounded-md bg-surface active:bg-surface-active border border-border" onClick={() => setIsOpen(false)}>
+      <BottomSheet isOpen={isOpen} size="auto" onClose={() => setIsOpen(false)}>
+        <div className="flex-1 overflow-y-auto">
+          <h2 className="text-lg font-bold">Filters</h2>
+          <div className="flex justify-between items-center mt-2 mb-4">
+            <p className="block mt-4 mb-1 font-medium">Muscle Group</p>
+            <select className="border border-border rounded-full p-2 focus:outline-none focus:border-accent">
+              <option value="">Select a muscle group</option>
+            </select>
+          </div>
+        </div>
+
+        {/* sticky buttons at the bottom */}
+        <div className="pt-4 mt-4 border-t border-border">
+          <div className="flex justify-center space-x-4">
+            <button
+              type="button"
+              className="px-2 py-1 rounded-md bg-surface active:bg-surface-active border border-border"
+              onClick={() => setIsOpen(false)}
+            >
               Close
             </button>
             {/* TODO - implement filter logic */}
-            <button type="button" className="px-2 py-1 rounded-md bg-accent active:bg-accent-active border border-border" onClick={() => alert("Apply filters")}>
+            <button
+              type="button"
+              className="px-2 py-1 rounded-md bg-accent active:bg-accent-active border border-border"
+              onClick={() => alert("Apply filters")}
+            >
               Apply
             </button>
           </div>
